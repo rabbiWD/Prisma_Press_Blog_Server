f443285ff2df1c88afa523468af372de8c3c8157
Rabbi Khan <Krabby2244@gmail.com>
2026-07-13 20:31:06 +0600

    add Prisma client and update server configuration

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/scripts/default-index.js";
+
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
