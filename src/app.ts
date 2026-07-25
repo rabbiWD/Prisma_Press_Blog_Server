18ad90accd6579c9660facc3b535bd5878153a15
Rabbi Khan <Krabby2244@gmail.com>
2026-07-25 19:19:54 +0600

    add global error handler and 404 middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, request, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.route";
 import { postRoutes } from "./modules/post/post.route";
 import { commentRoutes } from "./modules/comment/comment.route";
+import { notFound } from "./middlewares/notFound";
 
 // import { prisma } from "./lib/prisma";
+import  httpStatus  from 'http-status';
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -30,6 +33,27 @@ app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
+
+// app.use((req: Request, res: Response)=>{
+//     res.status(404).json({
+//         message: "Route not found",
+//         path: req.originalUrl,
+//         date: Date()
+//     })
+// })
+app.use(notFound)
+
+// app.use((err: any, req:Request, res: Response, next: NextFunction)=>{
+//     console.log(err)
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//         success: false,
+//         statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//         message: err.message,
+//         error: err.stack,
+//       });
+// })
+
+app.use(globalErrorHandler)
 
 
 
